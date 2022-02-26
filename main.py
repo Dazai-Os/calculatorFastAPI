1b0e5fa9bb6c4f7757c73271d193ed52e7c11094
Dazai-Os <vysokolyan2016@gmail.com>
2022-02-26 17:25:13 +0300

    add comments

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from models.calc_models import CalcPost
 from endpoints import calc
 from endpoints import history
 
-
+#Инициализируем app и подкючаем router к FastAPI
 app = FastAPI()
 app.include_router(calc.router)
 app.include_router(history.router)
 
-
+#Подключаемся к базе данных
 @app.on_event("startup")
 async def startup():
     await db.create()
     await db.create_table_users()
 
-
+#Запускаем на локальной машине uvicorn сервер на котором будет работать FastAPI
 if __name__ == "__main__":
     uvicorn.run("main:app", port = 8000, host = "127.0.0.1", reload = True)
